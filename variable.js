3da79a2f0ef2dd61e4957fbc48b0638ec5b60bed
hamzabwn <hamzahameedbwn@gmail.com>
2024-01-30 03:34:39 -0800

    update

---
+++ variable.js
@@ -0,0 +1,6 @@
+var eng =10; // var is a keyword for declear for variable
+let urdu = 20; // let is also for declear for variable
+const math = 40;
+ console.log(eng);
+ console.log(urdu);
+ console.log(math);
