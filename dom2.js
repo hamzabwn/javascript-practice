39c4c1390f4147394056452d6eb06203da8cb8fc
hamzabwn <hamzahameedbwn@gmail.com>
2024-02-01 10:46:39 -0800

    update

--- dom2.js
+++ dom2.js
@@ -9,19 +9,3 @@ console.log(link.textContent); // output show click me!
 console.log(link.innerHTML); // will show inner content with tags
 
 
-
-console.log("hellow");
-
-
-var names = document.getElementById("name");
-var email= document.getElementById("email");
-var pwd = document.getElementById("password");
-
-var submit = document.getElementById("submit");
-submit.onclick = function()
-{
-if(names.value!=null&&email.value!=null)
-{
-    document.write("success");
-}
-};
---
+++ dom_paraent_child.js
@@ -0,0 +1,5 @@
+
+console.log("hi hamza");
+var link = document.getElementById("link");
+document.write(link.parentNode);
+
