4df6c1fe8167bb7c3897a0f6809632950e59740f
hamzabwn <hamzahameedbwn@gmail.com>
2024-02-01 09:39:47 -0800

    new update

--- validation.js
+++ validation.js
@@ -1,15 +1,4 @@
 
-
-// Html file name is connect_js_to_html.html
-// we will get the value of html attributes by id 
-console.log("hellow");
-
-var link = document.getElementById("123");
-console.log(link.textContent); // output show click me!
-console.log(link.innerHTML); // will show inner content with tags
-
-
-
 console.log("hellow");
 
 
